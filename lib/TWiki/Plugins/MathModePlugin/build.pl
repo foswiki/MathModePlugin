bde7c5c11a94f83ab812c0da9025cf7109d5cfc6
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-07 17:23:41 +0000

    Item8039: fixed build script
    git-svn-id: http://svn.foswiki.org/trunk/MathModePlugin@1816 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/MathModePlugin/build.pl
+++ lib/TWiki/Plugins/MathModePlugin/build.pl
@@ -6,10 +6,10 @@ BEGIN {
     unshift @INC, split(/:/, $ENV{FOSWIKI_LIBS});
 }
 
-use TWiki::Contrib::Build;
+use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new TWiki::Contrib::Build( 'MathModePlugin' );
+$build = new Foswiki::Contrib::Build( 'MathModePlugin' );
 
 # Build the target on the command line, or the default target
 $build->build($build->{target});
