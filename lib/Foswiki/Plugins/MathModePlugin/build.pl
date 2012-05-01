0906d1e1af29fdd21b3a4bb00f92db48137ef754
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/MathModePlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/MathModePlugin/build.pl
+++ lib/Foswiki/Plugins/MathModePlugin/build.pl
@@ -3,14 +3,14 @@
 # Build for MathModePlugin
 #
 BEGIN {
-    unshift @INC, split(/:/, $ENV{FOSWIKI_LIBS});
+    unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
 }
 
 use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new Foswiki::Contrib::Build( 'MathModePlugin' );
+$build = new Foswiki::Contrib::Build('MathModePlugin');
 
 # Build the target on the command line, or the default target
-$build->build($build->{target});
+$build->build( $build->{target} );
 
